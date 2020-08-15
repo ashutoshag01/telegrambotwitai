0560d6ddb8304a3a281ec6790eca92b5671ea9da
black-barbarian <ayushijain1104@gmail.com>
2020-08-15 15:40:59 +0530

    app.py commit

--- app.py
+++ app.py
@@ -1,86 +1,58 @@
-import re
-from flask import Flask, request
-import telegram
-from telegram.ext import Updater, CommandHandler
-import logging
+#import re
+#from flask import Flask, request
+#import telegram
 #from telegram.passport.credentials import bot_token, bot_user_name,URL
 
+#imports
+import os
+import logging
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
 """bot credentials"""
-bot_token="1174443046:AAH39jAknodjzwQkXVtsQtME0j0tY2ZuIx8"
-bot_user_name="Talkbuddy_bot"
-URL="https://talkbuddybot.herokuapp.com"
+TOKEN = "1174443046:AAH39jAknodjzwQkXVtsQtME0j0tY2ZuIx8"
+PORT = int(os.environ.get('PORT','8443'))
+BOT_USER_NAME = "Talkbuddy_bot"
+URL = "https://talkbuddybot.herokuapp.com/"
 
 
-global bot
-global TOKEN
-TOKEN = bot_token
+#Enable Logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-bot = telegram.Bot(token=TOKEN)
-updater = Updater(token=TOKEN, use_context=True)
-dispatcher = updater.dispatcher
+#Define few commands handlers
+def start(update, context):
+   """When /start is hit"""
+   update.message.reply_text('Hi!')
 
-logging.basicConfig(format='%(sctiem)s - %(name)s - %(levelname)s - %(message)s',
-                                level=logging.INFO)
+def help_command(update, context):
+   """When /help is hit"""
+   update.message.reply_text('Help!')
 
-def start(update, context): context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+def echo(update, context):
+   """Echo the user message."""
+   update.message.reply_text(update.message.text)
 
-updater.start_polling()
+def main():
+   """start the bot"""
+   updater = Updater(TOKEN, use_context=True)
 
-#app = Flask(__name__)
+   #get the dispatcher to register handlers
+   dp = updater.dispatcher
 
-'''
-@app.route('/{}'.format(TOKEN), methods=['POST'])
-def respond():
-   # retrieve the message in JSON and then transform it to Telegram object
-   update = telegram.Update.de_json(request.get_json(force=True), bot)
+   #on different commands answer in telegram
+   dp.add_handler(CommandHandler("start",start))
+   dp.add_handler(CommandHandler("help",help_command))
 
-   chat_id = update.message.chat.id
-   msg_id = update.message.message_id
+   #to reply the same text back
+   dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
-   # Telegram understands UTF-8, so encode text for unicode compatibility
-   text = update.message.text.encode('utf-8').decode()
-   # for debugging purposes only
-   print("got text message :", text)
-   # the first time you chat with the bot AKA the welcoming message
-   if text == "/start":
-       # print the welcoming message
-       bot_welcome = """
-       Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
-       """
-       # send the welcoming message
-       bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
-
-
-   else:
-       try:
-           # clear the message we got from any non alphabets
-           text = re.sub(r"\W", "_", text)
-           # create the api link for the avatar based on http://avatars.adorable.io/
-           url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
-           # reply with a photo to the name the user sent,
-           # note that you can send photos by url and telegram will fetch it for you
-           bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
-       except Exception:
-           # if things went wrong
-           bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
-
-   return 'ok'
-
-@app.route('/set_webhook', methods=['GET', 'POST'])
-def set_webhook():
-   s = bot.setWebhook(URL)
-   if s:
-       return "webhook setup ok"
-   else:
-       return "webhook setup failed"
-
-@app.route('/')
-def index():
-   return '.'
-
+   #start the bot 
+   updater.start_webhook(listen = "0.0.0.0",
+                         port = PORT,
+                         url_path = TOKEN)
+   updater.bot.set_webhook(URL+TOKEN)
+   updater.idle()
 
 if __name__ == '__main__':
-   app.run(threaded=True)
-'''
+   main()
